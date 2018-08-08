241f55a5b450bfb1c61a16814641441e95691143
volosgoto <volosgoto@yahoo.com>
2018-08-08 09:37:52 +0300

    add event listener

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ function Book(title, author, isbn) {
 // UI COnstrucror
 function Ui() {   
 }
+
+
+// Event listeners
+document.getElementById('book-form').addEventListener('submit', 
+    function (e) {
+        
+        let title = document.getElementById('title').value;
+        let author = document.getElementById('author').value;
+        let isbn = document.getElementById('isbn').value;
+        console.log(title, author, isbn);
+
+        e.preventDefault();
+    }
+);
