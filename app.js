84849a13555d4d665d310e74034f542c1cd44022
volosgoto <volosgoto@yahoo.com>
2018-08-08 10:02:31 +0300

    instanciate book and ui

--- app.js
+++ app.js
@@ -9,16 +9,32 @@ function Book(title, author, isbn) {
 function Ui() {   
 }
 
+// Add book to list
+Ui.prototype.addBookToList = function (book) {
+    
+}
+
+
 
 // Event listeners
 document.getElementById('book-form').addEventListener('submit', 
     function (e) {
-        
+        // Get form values
         let title = document.getElementById('title').value;
         let author = document.getElementById('author').value;
         let isbn = document.getElementById('isbn').value;
-        console.log(title, author, isbn);
 
+        // Instanciate book
+        let book = new Book(title, author, isbn);
+
+        // Instanciate Ui
+        let ui = new Ui();
+
+        // Add book to list
+        ui.addBookToList(book);
+
+        console.log(book); 
+        
         e.preventDefault();
     }
 );
