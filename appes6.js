313ed84a134740e49386b5b585918000f6b0eeaf
andrey <volosgoto@yahoo.com>
2018-08-09 14:50:29 +0300

    convert to ES6 classes

---
+++ appes6.js
@@ -0,0 +1,27 @@
+// ES6
+
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+class Ui {
+    addBookToList() {
+
+    }
+
+    showAlert() {
+
+    }
+
+    deleteBook() {
+
+    }
+
+    clearFields() {
+
+    }
+}
